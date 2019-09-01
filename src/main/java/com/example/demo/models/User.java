59ca9c6eb6e102f29c5ca51c7ccf4d48cc90e240
Fabrice Touopi <bruneltouopi@yahoo.fr>
2019-09-01 15:44:34 +0200

    init project

---
+++ src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,14 @@
+package com.example.demo.models;
+
+/**
+ * Created by fabrice on 9/1/19.
+ */
+public class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
